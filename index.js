ac00fe45d0fc72ca53a64f336aa9e8185820ce61
Muskan326 <muskancs0037@gmail.com>
2020-09-22 18:52:14 +0530

    Final

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const globalErrorMiddleware = require('./app/middlewares/appErrorHandler')
 const app = express()
 var cors = require('cors')
 
-app.use(cors())
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    next();
+});
 
 //middlewares
 app.use(bodyParser.json())
